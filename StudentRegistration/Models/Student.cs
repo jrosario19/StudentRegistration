f2b7a3b467dbae83a4cc7a5f5b1a55d3d1ec3d9e
jrosario19@hotmail.com <jrosario19@hotmail.com>
2020-02-11 08:45:27 -0400

    Agregar archivos de proyecto.

---
+++ StudentRegistration/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentRegistration.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        [Required (ErrorMessage ="El nombre es un campo obligatorio")]
+        [Display(Name ="Nombre")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "El apellido es un campo obligatorio")]
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "El estatus es un campo obligatorio")]
+        [Display(Name = "Estatus")]
+        public bool Status { get; set; }
+        [Required(ErrorMessage = "La carrera es un campo obligatorio")]
+        [Display(Name = "Carrera")]
+        public string Career { get; set; }
+    }
+}
---
+++ StudentRegistration/Models/StudentDBContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StudentRegistration.Models
+{
+    public partial class StudentDBContext : DbContext
+    {
+        //public StudentDBContext()
+        //{
+        //}
+
+        public StudentDBContext(DbContextOptions<StudentDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=(localDB)\\MSSQLLocalDB;database=StudentDB;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Career)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
